1e9c864cef2e0597f1804c425abf7f6c6aa93b93
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-22 21:29:40 +0300

    list view

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('main_menu/', view=views.main_menu, name='main_menu'),
     # Category CRUD
     # path('category/create/', view=views.category_create, name='category_create'),
-    # path('category/list/', view=views.category_list, name='category_list'),
+    path('category/list/', view=views.category_list, name='category_list'),
     # path('category/<int:pk>/edit/', view=views.category_edit, name='category_edit'),
     # path('category/<int:pk>/delete/', view=views.category_delete, name='category_delete'),
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from .models import Category
+
+
 # Create your views here.
 
 
 def main_menu(request: HttpRequest) -> HttpResponse:
     return render(request, 'recipes/main_menu.html')
+
+
+#################### CATEGORIES
+def category_list(request: HttpRequest) -> HttpResponse:
+    categories = Category.objects.all()
+    return render(request, 'recipes/category_list.html', {'categories': categories})
