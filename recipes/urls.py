dd2b0c2e52756b6faa94f457a567c273713dad7d
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-22 12:27:06 +0300

    views urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,12 @@
 
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 app_name = 'recipes'
 urlpatterns = [
-
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/create/', views.category_create, name='category_create'),
+    path('categories/<int:pk>/update/', views.category_update, name='category_update'),
+    path('categories/<int:pk>/delete/', views.category_delete, name='category_delete'),
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import CategoryForm
+from .models import Category
+
 
 # Create your views here.
+
+# CREATE
+def category_create(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'recipes/category_form.html', {'form': form})
+
+
+# READ - list
+def category_list(request: HttpRequest) -> HttpResponse:
+    categories = Category.objects.all()
+    return render(request, 'recipes/category_list.html', {'categories': categories})
+
+
+# UPDATE
+def category_update(request: HttpRequest, pk: int) -> HttpResponse:
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'recipes/category_form.html', {'form': form})
+
+
+# DELETE
+def category_delete(request: HttpRequest, pk: int) -> HttpResponse:
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'recipes/category_confirm_delete.html', {'category': category})
