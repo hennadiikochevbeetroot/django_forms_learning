face9993cb5f23828cf2c3c48322f914d9eb39ae
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-22 20:57:58 +0300

    Main menu view

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+
+def main_menu(request: HttpRequest) -> HttpResponse:
+    return render(request, 'recipes/main_menu.html')
