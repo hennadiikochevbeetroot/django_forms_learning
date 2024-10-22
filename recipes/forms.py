232e22bbdf4a4190f2d142c72a31ff37453866f0
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-22 12:11:01 +0300

    category form

---
+++ recipes/forms.py
@@ -0,0 +1,17 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django import forms
+
+from .models import Category
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('submit', 'Save Category'))
