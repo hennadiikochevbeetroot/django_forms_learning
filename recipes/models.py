40e5efcb929aa66986267386480948e1837cdb5b
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-24 00:47:07 +0300

    add user reference to all of em

--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(verbose_name='Category Name', max_length=100)
     description = models.TextField(verbose_name='Category Description')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +18,8 @@ class Ingredient(models.Model):
     type = models.CharField(verbose_name='Ingredient Type', max_length=50)
     cost_per_kg = models.FloatField(verbose_name='Ingredient Cost Per KG')
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.name
 
@@ -25,6 +29,8 @@ class Recipe(models.Model):
     content = models.TextField(verbose_name='Recipe Content')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     ingredients = models.ManyToManyField(Ingredient)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
