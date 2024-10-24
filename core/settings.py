a9efaee2b5862b770ed24a430aa01aa982bb82e8
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-24 20:37:28 +0300

    user reference

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'recipes.apps.RecipesConfig',
 ]
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,6 +7,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(verbose_name='Category Name', max_length=100)
     description = models.TextField(verbose_name='Category Description')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +19,8 @@ class Ingredient(models.Model):
     type = models.CharField(verbose_name='Ingredient Type', max_length=50)
     cost_per_kg = models.FloatField(verbose_name='Ingredient Cost Per KG')
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.name
 
@@ -25,6 +30,8 @@ class Recipe(models.Model):
     content = models.TextField(verbose_name='Recipe Content')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     ingredients = models.ManyToManyField(Ingredient)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
