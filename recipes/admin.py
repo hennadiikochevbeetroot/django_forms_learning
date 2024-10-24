3fd71bff749febb065a47cd7392cb269c0f4a433
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-24 13:25:19 +0300

    admin views

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 
-from .models import Recipe, Ingredient, Category
+from .models import Category, Ingredient, Recipe
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General', {'fields': ['name', 'description']}),
+        ('User Reference', {'fields': ['user']}),
+    ]
+
+
+# class IngredientInline(admin.StackedInline):
+#     model = Ingredient
+#     extra = 3
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General', {'fields': ['name', 'content']}),
+        ('Foreign Keys', {'fields': ['category', 'ingredients']}),
+    ]
+
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
