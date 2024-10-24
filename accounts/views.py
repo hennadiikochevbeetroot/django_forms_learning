da54bbbd37bea70d8a7d0bc2c22763c518103ca4
hennadiikochevbeetroot <hennadiikochevbeetroot@gmail.com>
2024-10-24 21:35:30 +0300

    register works

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
 
-def register(request: HttpRequest) -> HttpResponse:
-    return HttpResponse()
 
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('recipes:main_menu')
+    else:
+        form = UserCreationForm()
+
+    return render(request, template_name='accounts/register.html', context={'form': form})
